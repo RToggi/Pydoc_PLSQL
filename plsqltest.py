2d920c4bdb30da27aacf41e2437d2b83f3443e2d
Rahul Toggi <10651541@INFBASZD10181.local>
2019-02-21 12:16:58 +0530

    Only comments

--- plsqltest.py
+++ plsqltest.py
@@ -2,26 +2,31 @@ import FileReadWrite
 
 contents = FileReadWrite.ReadWriteobj.file_read("pydocteradata.sql")
 
-foundq = 0
-# comments = contents.split("\"\"\"")
-# for i in range(len(comments)):
-#     comments[i] = comments[i].strip()
-txt = ""
-print contents
-comments = []
-for i in contents:
-    if "\"\"\"" in i:
-        foundq = foundq + 1
-    if foundq == 1:
-        comments.append(i)
-    if foundq == 2:
-        foundq = 0
 
-print "Only Comments"
+def pick_comments():
+    foundq = 0
+    # print contents
+    comments = []
+    for i in contents:
+        if "\"\"\"" in i:
+            foundq = foundq + 1
+        if foundq == 1:
+            if "\"\"\"" not in i:
+                comments.append(i)
+        if foundq == 2:
+            foundq = 0
 
-# for i in range(len(comments)):
-#     comments[i] = comments[i].strip()
-# print comments
+    FileReadWrite.ReadWriteobj.file_write("Ouput.txt", comments)
+    # print len(comments)
 
-FileReadWrite.ReadWriteobj.file_write("Ouput.txt", comments)
-print len(comments)
+
+def parse_json():
+    pass
+
+
+def parse_txt():
+    pass
+
+
+pick_comments()
+parse_txt() 
